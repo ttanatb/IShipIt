1949463a55433e14c016d72cb7c3c9f936cc396e
svaswani <svaswani80@gmail.com>
2017-11-16 09:37:55 -0500

    Updated ship controls

--- IShipIt/IShipIt/MotionMonitor.swift
+++ IShipIt/IShipIt/MotionMonitor.swift
@@ -32,13 +32,13 @@ class MotionMonitor {
                     return
                 }
                 
-                self.rotation = CGFloat(atan2(data!.gravity.x, data!.gravity.y)) - CGFloat.pi
+                self.rotation = CGFloat(atan2(data!.gravity.x, data!.gravity.y)) - CGFloat.pi / 2
                 self.gravityVecNormalized = CGVector(dx:CGFloat(data!.gravity.x), dy:CGFloat(data!.gravity.y))
                 self.gravityVec = CGVector(dx:CGFloat(data!.gravity.x), dy:CGFloat(data!.gravity.y)) * 9.8
                 
                 self.transform = CGAffineTransform(rotationAngle: CGFloat(self.rotation))
-                print("self.rotation = \(self.rotation)")
-                print("self.gravityVectorNormalized = \(self.gravityVecNormalized)")
+                //print("self.rotation = \(self.rotation)")
+                //print("self.gravityVectorNormalized = \(self.gravityVecNormalized)")
             }
         } else {
             print("Device Motion is not available! Are you on the simulator?")
